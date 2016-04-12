03cfc18f3722cc29bb0403d8a1ffce642fbe09f3
winecat <winecat@qq.com>
2016-04-13 01:01:43 +0800

    update file

--- scripts/lnmp_install.sh
+++ scripts/lnmp_install.sh
@@ -1,1 +1,26 @@
 #!/usr/bin/env sh
+
+cd `pwd`
+
+echo "installing depend packages, please wait..."
+sh /tools/install_packages.sh
+
+if [ $? -eq 0 ]
+	then 
+		echo "lnmp depend packages install sucessfull"
+else
+	echo "lnmp depend packages install fail"
+fi
+
+echo "installing php library files, please wait..."
+
+sh /tools/install_php_lib.sh
+
+if [ $? -eq 0 ]
+	then 
+		echo "php library install sucessfull"
+else
+	echo "php library install fail"
+fi
+
+echo "installing php library files..."
